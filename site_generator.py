d61f808dafcb2e153d79ef84d00982186c07a31c
Bongani <1techjunky@gmail.com>
2014-11-24 05:49:30 -0600

    tagging and freezon state added

--- site_generator.py
+++ site_generator.py
@@ -1,6 +1,10 @@
+import sys
+
+
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask import render_template
+from flask_frozen import Freezer
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -9,12 +13,18 @@ FLATPAGES_EXTENSION = ".md"
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
+freezer = Freezer(app)
 
 
 @app.route('/')
-@app.route('/<name>')
-def index(name=None):
+def index():
     return render_template('index.html', pages=pages)
+
+
+@app.route('/tag/<string:tag>/')
+def tag(tag):
+    tagged = [p for p in pages if tag in p.meta.get('tags', [])]
+    return render_template('tag.html', pages=tagged, tag=tag)
 
 
 @app.route('/blog/<path:path>/')
@@ -23,4 +33,7 @@ def page(path):
     return render_template('page.html', page=page)
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
